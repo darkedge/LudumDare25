10a4fb041cf885212b6867dec211713a14499fe2
Marco Jonkers <marcojonkers@gmail.com>
2012-12-15 02:01:41 +0100

    Game loop and Input work

--- src/ld25/Game.java
+++ src/ld25/Game.java
@@ -2,6 +2,7 @@ package ld25;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferStrategy;
 
@@ -13,14 +14,19 @@ public class Game extends Canvas implements Runnable {
 	private static final String TITLE = "TODO: Set name!";
 	private static final int WIDTH = 640;
 	private static final int HEIGHT = 480;
+	private static final int TICK_RATE = 60;
 	
 	private long lastTime;
 	private boolean running = false;
+	private int fps;
+	private int fpsTimer;
 	
 	public Game() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setIgnoreRepaint(true);
 		addKeyListener(Input.INSTANCE);
+		addMouseMotionListener(Input.INSTANCE);
+		addMouseListener(Input.INSTANCE);
 	}
 	
 	public void start() {
@@ -34,18 +40,28 @@ public class Game extends Canvas implements Runnable {
 	@Override
 	public void run() {
 		running = true;
-		long lastTime = System.currentTimeMillis();
-		float delta = 0;
+		double accumulator = 0;
+		final double TICK_TIME = 1000000000d / TICK_RATE;
+		long lastTime = System.nanoTime();
 		
-		while(running) {
-			// Timing
-			long now = System.currentTimeMillis();
-			delta += GameMath.clamp(now - lastTime, 0, 50);
-			lastTime = now;
+		while (running) {
+			long now = System.nanoTime();
+			long delta = now - lastTime;
+			delta = GameMath.clamp(delta, 0, 100000000);
+			accumulator += delta;
 			
-			while (delta > 0) {
-				delta -= 1000.0f / 60;
+			while (accumulator > TICK_TIME) {
 				tick();
+				accumulator -= TICK_TIME;
+			}
+
+			render(accumulator);
+			fps++;
+			
+			if (System.currentTimeMillis() - lastFpsTime > 1000) {
+				lastFpsTime += 1000;
+				System.out.println(fps + " fps");
+				fps = 0;
 			}
 			
 			Thread.yield();
@@ -61,12 +77,16 @@ public class Game extends Canvas implements Runnable {
 	
 	private void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
 		
-		
+		Graphics g = bs.getDrawGraphics();
+		g.fillRect(0, 0, getWidth(), getHeight());
+		screen.render(g);
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
--- src/ld25/Input.java
+++ src/ld25/Input.java
@@ -6,51 +6,126 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public enum Input implements KeyListener, MouseListener, MouseMotionListener{
     INSTANCE;
     private static final int RANGE = 256;
     
-    private HashSet<Integer> pressed = new HashSet<Integer>(RANGE);
-    private HashSet<Integer> released = new HashSet<Integer>(RANGE);
-    private HashSet<Integer> held = new HashSet<Integer>(RANGE);
-    private HashSet<Integer> newreleased = new HashSet<Integer>(RANGE);
+    private static HashSet<Integer> pressed = new HashSet<Integer>(RANGE);
+    private static HashSet<Integer> released = new HashSet<Integer>(RANGE);
+    private static HashSet<Integer> held = new HashSet<Integer>(RANGE);
+    private static HashSet<Integer> newreleased = new HashSet<Integer>(RANGE);
+    
+    public static enum Button {
+		QUIT, ACCEPT, LEFT, RIGHT, UP, DOWN, FIRE;
+	}
+    
+    private static HashMap<Integer, Button> MAP = new HashMap<Integer, Button>();
+    
+    // Mouse
+    private static int x = 0;
+    private static int y = 0;
+    private static int dx = 0;
+    private static int dy = 0;
+    private static int newx = 0;
+    private static int newy = 0;
+    
+    static {
+    	MAP.put(KeyEvent.VK_LEFT, Button.LEFT);
+		MAP.put(KeyEvent.VK_RIGHT, Button.RIGHT);
+		MAP.put(KeyEvent.VK_UP, Button.UP);
+		MAP.put(KeyEvent.VK_DOWN, Button.DOWN);
+		MAP.put(KeyEvent.VK_SPACE, Button.FIRE);
+		MAP.put(KeyEvent.VK_CONTROL, Button.FIRE);
+		MAP.put(KeyEvent.VK_SPACE, Button.FIRE);
+		MAP.put(KeyEvent.VK_Z, Button.FIRE);
+		MAP.put(KeyEvent.VK_X, Button.FIRE);
+		MAP.put(KeyEvent.VK_ENTER, Button.ACCEPT);
+    }
     
     public static void tick() {
-    	INSTANCE.pressed.removeAll(INSTANCE.held);
-    	INSTANCE.held.addAll(INSTANCE.pressed);
-    	INSTANCE.held.removeAll(INSTANCE.released);
+    	Input.pressed.removeAll(Input.held);
+    	Input.held.addAll(Input.pressed);
+    	Input.held.removeAll(Input.released);
     	
-    	INSTANCE.released.clear();
-    	INSTANCE.released.addAll(INSTANCE.newreleased);
-    	INSTANCE.newreleased.clear();
+    	Input.released.clear();
+    	Input.released.addAll(Input.newreleased);
+    	Input.newreleased.clear();
+    	
+    	dx = newx - x;
+    	dy = newy - y;
+    	x = newx;
+    	y = newy;
+    }
+    
+    public static int getMouseX() {
+    	return Input.x;
+    }
+    
+    public static int getMouseY() {
+    	return Input.y;
+    }
+    
+    public static int getMouseDX() {
+    	return Input.dx;
+    }
+    
+    public static int getMouseDY() {
+    	return Input.dy;
+    }
+    
+    public static boolean getButton(Button button) {
+    	for(int i : Input.MAP.keySet()) {
+    		if(Input.MAP.get(i) == button && Input.held.contains(i)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public static boolean getButtonDown(Button button) {
+    	for(int i : Input.MAP.keySet()) {
+    		if(Input.MAP.get(i) == button && Input.pressed.contains(i)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    public static boolean getButtonUp(Button button) {
+    	for(int i : Input.MAP.keySet()) {
+    		if(Input.MAP.get(i) == button && Input.released.contains(i)) {
+    			return true;
+    		}
+    	}
+    	return false;
     }
     
     public static boolean getKey(int key) {
-    	return INSTANCE.held.contains(key);
+    	return Input.held.contains(key);
     }
     
     public static boolean getKeyDown(int key) {
-    	return INSTANCE.pressed.contains(key);
+    	return Input.pressed.contains(key);
     }
     
     public static boolean getKeyUp(int key) {
-    	return INSTANCE.released.contains(key);
+    	return Input.released.contains(key);
     }
     
     public static Collection<Integer> getKeys() {
-    	return INSTANCE.held;
+    	return Input.held;
     }
     
     public static Collection<Integer> getKeysDown() {
-    	return INSTANCE.pressed;
+    	return Input.pressed;
     }
     
     public static Collection<Integer> getKeysUp() {
-    	return INSTANCE.released;
+    	return Input.released;
     }
-    
     
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -65,46 +140,41 @@ public enum Input implements KeyListener, MouseListener, MouseMotionListener{
 	}
 	
 	@Override
-	public void keyTyped(KeyEvent arg0) {}
+	public void keyTyped(KeyEvent e) {}
 
 	@Override
-	public void mouseDragged(MouseEvent arg0) {
+	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent arg0) {
+	public void mouseMoved(MouseEvent e) {
+		newx = e.getX();
+		newy = e.getY();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// Mouse entered game area
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// Mouse left game area
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
